cef960a131cc141f21504faa437a7efb963465d9
Logan Mann <loganmann8@gmail.com>
2022-04-06 14:26:00 -0700

    Added frontend client project

---
+++ finalprojectclient/src/main/java/com/naiflogan/finalproject/client/App.java
@@ -0,0 +1,17 @@
+package com.naiflogan.finalproject.client;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
