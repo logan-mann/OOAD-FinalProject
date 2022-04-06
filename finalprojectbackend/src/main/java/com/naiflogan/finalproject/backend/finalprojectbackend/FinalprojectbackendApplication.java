a87f5a9074c562ad7e4b6c770cdfb7fc5d154c1d
Logan Mann <loganmann8@gmail.com>
2022-04-06 12:04:16 -0700

    initial commit

---
+++ finalprojectbackend/src/main/java/com/naiflogan/finalproject/backend/finalprojectbackend/FinalprojectbackendApplication.java
@@ -0,0 +1,13 @@
+package com.naiflogan.finalproject.backend.finalprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalprojectbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalprojectbackendApplication.class, args);
+	}
+
+}
---
+++ finalprojectbackend/src/test/java/com/naiflogan/finalproject/backend/finalprojectbackend/FinalprojectbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naiflogan.finalproject.backend.finalprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
